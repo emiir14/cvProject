b67692845579977bbbef90db8dd98f2c9c92f51f
Emir Ale <emirderespaldo@gmail.com>
2019-09-22 23:31:48 -0300

    modify index by changing app.use again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static(__dirname + '/public'));
+app.use('/static', express.static('public'));
 
 server.listen(PORT, function() {
   console.log('CV Runing');
